1172368f5437cca8614bd0d704ccbadb1a8f74af
Joël Bosch <studielink@joelbosch.nl>
2021-08-29 23:55:57 +0200

    Cleaned up some comments

--- api/server.py
+++ api/server.py
@@ -9,11 +9,11 @@ This is the main file that runs the API service.
 """
 Main
 
-This is the main process that is called when this file is opened
-explicitly.
+This is the main process that is called when this file is opened explicitly.
 """
 def main():
     ...
+
 
 # Check if this file was opened explicitly to execute and only then run the main
 # function. This way, we can prevent the runtime execution of this program when
