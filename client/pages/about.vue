b18a16e86a552bc73313b7c6107060e333830911
Joël Bosch <studielink@joelbosch.nl>
2022-01-21 15:58:51 +0100

    Improved texts

--- client/pages/about.vue
+++ client/pages/about.vue
@@ -4,15 +4,15 @@
       About Reachable
     </h1>
     <p class="body-1">
-      Reachable was creating to allow for easy communication without sharing
+      Reachable was created to allow for easy communication without sharing
       your contact details! You can use Reachable to quickly create forms that
       anyone can use to send you an email - without them knowing your email
       address! All they need is a link or QR code to find your form.
     </p>
     <p class="body-1">
       Every email you get from one of our forms has a button that lets you
-      disable the form and cut off communication on <em>your</em> terms. This way,
-      you can be open for communication while you stay 100% in control!
+      disable the form and cut off communication on <em>your</em> terms. This
+      way, you can be open for communication while you stay 100% in control!
     </p>
   </v-container>
 </template>
