5888c09fefb51f5b8ba50daeeb2dac3674117813
Joël Bosch <studielink@joelbosch.nl>
2022-01-21 15:49:41 +0100

    Added background animation documentation

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -12,6 +12,7 @@
 /* Long lines of text are hard to read, so we like to keep them short. */
 .text { max-width: 640px; }
 
+/* Create a subtle animated background to make design more interesting. */
 .animated-background {
   background: linear-gradient(
     -45deg,
@@ -23,10 +24,11 @@
     #DCF1D0
   );
   background-size: 1000% 1000%;
-  animation: gradient 20s ease infinite;
+  animation: back-and-forth 20s ease infinite;
   height: 80%;
 }
-@keyframes gradient {
+/* Simple back and forth animation for the background. */
+@keyframes back-and-forth {
   0% { background-position: 0% 50%; }
   50% { background-position: 100% 50%; }
   100% { background-position: 0% 50%; }
